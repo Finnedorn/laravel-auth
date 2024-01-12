492220a81397d790aa69eb730a8a4698bb059aab
Finnedorn <daniele.giuntoli@outlook.com>
2024-01-12 16:41:04 +0100

    added basic scaffholding

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\ComicController;
+use App\Http\Controllers\Admin\ProjectController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -23,7 +23,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    //Route::resource('comics', ComicController::class);
+    Route::resource('projects', ProjectController::class);
 });
 
 
